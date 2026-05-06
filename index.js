a8c3d1d42556735f3619d8994d02a931e124f757
Omkar Sharma <104587783+omkarsharma2821@users.noreply.github.com>
2026-05-06 09:57:21 +0530

    Update server response message to version 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello from the server v2!');
 });
 
 app.listen(PORT, () => {
